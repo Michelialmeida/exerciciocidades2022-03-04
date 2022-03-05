380a8b8a9efd4caa7dfe497d288cb45534216096
Michelialmeida <miihrusso2017@gmail.com>
2022-03-04 22:17:25 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("São Vicente");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Santos");
+Console.WriteLine("Peruibe");
+
+
+
+
